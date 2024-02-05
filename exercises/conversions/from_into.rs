c8a500173a9878ff3ff9aaf97250658e675565c3
donkey <anjingyu_ws@foxmail.com>
2024-02-05 22:06:58 +0800

    feat: completed the 77th-100th exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let splitted: Vec<&str> = s.split(",").collect();
+        if splitted.len() != 2 {
+            return Person::default();
+        }
+
+        if splitted[0].len() == 0 || splitted[1].len() == 0 {
+            return Person::default();
+        }
+
+        let age = splitted[1].parse::<usize>();
+
+        match age {
+            Ok(x) => Person {
+                name: String::from(splitted[0]),
+                age: x,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
